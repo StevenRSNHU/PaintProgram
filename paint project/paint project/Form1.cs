2f767a75de665f5e7c8a8118fbbc5b169259f241
StevenRSNHU <60667716+StevenRSNHU@users.noreply.github.com>
2020-03-05 11:15:47 -0500

    added radio buttons

---
+++ paint project/paint project/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShapeCBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ShapeCBox.Items.Add("Rectangle");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
